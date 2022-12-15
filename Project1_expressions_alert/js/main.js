4544f251569b47b530337dfc23e4858dbfad0811
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2022-12-15 16:16:40 -0600

    Created website with Basic Javascript

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,17 @@
+window.alert("Hello, World!") //creates alert that displays text
+
+document.write("Hello, World!") //displays text on page
+
+var a= " This is a string."; //creates and defines a variable with the value of the text
+document.write(a) //displays text assigned to the variable
+
+document.write(" \"Shannon\'s Website\"" //displays text on page
++" is my website. ") //displays strings together
+
+var b= "Concatenated" + " String" //creates and defines the variables
+document.write(b) //concatenates and displays the strings
+
+var cat="Goose", dog="Murphy", lizard="Flint"; //creates and defines the variables
+document.write(dog); //displays the value of the variable dog
+
+document.write(2 + 2) //solves the expression and returns result
